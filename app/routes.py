2ecb1aea9bdb967ec19d1f30a6fd56d217bcd08b
masaboe <masaboe@gmail.com>
2021-12-07 11:29:23 +0700

    update konten

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Abu Salam'}
+    user = {'username': 'Abu Salam Semarang'}
     posts = [
         {
             'author': {'username': 'Jono'},
